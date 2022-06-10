cb48919f49b30d65d143bfab9915d219b0027c64
Orlando <orlando@dataops.co.uk>
2022-06-10 17:26:40 +0100

    Math.JS library integrated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-//Here we're importing items we'll need. You can add other imports here.
+// Import the required functions from teh Math.JS library
+const math = require('mathjs')
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+// Call the match funtion passing in the expression from FileMaker
+window.callMath = function (json) {
+  const obj = JSON.parse(json);
+  const expression = obj.expression;
 
-//
+  const result = math.evaluate(expression);
+
+  // Debug: Eitehr an alert or console.log the result of teh expression
+  // alert(result)
+  // console.log (result)
+
+  // Pass back the result to FileMaker
+  FileMaker.PerformScript ( "set Result" , JSON.stringify ( result ) ) 
+
+}
